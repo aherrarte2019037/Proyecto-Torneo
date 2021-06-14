559d51b784df95573016d12aa4b0d40529b83f84
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-06-14 15:19:09 -0600

    Se terminó la vista login

--- Frontend/src/app/pages/login-page/login-page.component.ts
+++ Frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  loginForm: FormGroup = this.buildForm();
 
-  constructor() { }
+  constructor( private fmBuilder: FormBuilder ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  buildForm() {
+    return this.fmBuilder.group({
+      'email': ['', [Validators.required, Validators.email]],
+      'pass' : ['', Validators.required]
+    })
+  }
+
+  login() {
+
   }
 
 }
