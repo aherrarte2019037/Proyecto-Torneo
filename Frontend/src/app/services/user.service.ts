9687269d54b2ba32436233025bc948a455f66e49
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-07-01 17:30:57 -0600

    Se editó app-routing-module

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,19 @@ export class UserService {
 
   login( username: string, password: string) {
     return this.http.post<any>( `${this.apiUrl}/login`, { password, username, getToken: true } )
+      .pipe( tap( data => this.setToken(data.token) )  )
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+  }
+
+  setToken( token: string ) {
+    localStorage.setItem('token', token)
+  }
+
+  isLogged() {
+    return localStorage.getItem('token')? true:false;
   }
 
 }
