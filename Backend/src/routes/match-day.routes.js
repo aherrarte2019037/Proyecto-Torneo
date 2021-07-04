42fb8688d2bf54a8e5737ad9897449d759c35a02
José Góngora <jdavidgongorao@gmail.com>
2021-07-04 15:53:54 -0600

    Se hizo la función de editar fecha y  asignar partidos en jornadas

--- Backend/src/routes/match-day.routes.js
+++ Backend/src/routes/match-day.routes.js
@@ -7,5 +7,7 @@ const md_authentication = require('../middlewares/authenticated');
 var api = express.Router();
 
 api.post('/createMatchDay/:LeagueId', md_authentication.ensureAuth, matchDayController.createMatchDay);
+api.put('/changeDate/:idMatchDay', md_authentication.ensureAuth, matchDayController.changeDate)
+api.get('/assignTeams/:idMatchDay', md_authentication.ensureAuth, matchDayController.assignTeams)
 
 module.exports = api;
